926dc725d3473977ec4f542ba4a36f610da3f612
evanjmg <evanjmg@gmail.com>
2015-09-08 20:46:49 +0100

    got search working

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
