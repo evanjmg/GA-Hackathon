a5e285a6c1012ec1a90bb932fd7c9fda84bf9330
evanjmg <evanjmg@gmail.com>
2015-09-10 23:29:17 +0100

    fixed some errors for deploy

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
